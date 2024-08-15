a878716728f40c36db4099ff04f71c1cf3e0aa67
vishnu varikuppla <vishnusvh888@gmail.com>
2024-08-15 11:08:42 +0530

    Learning JSX

---
+++ learning jsx/index.js
@@ -0,0 +1,24 @@
+// JavaScript code
+// const heading = document.createElement("h2");
+// heading.textContent = "Hello World";
+// heading.className ="header";
+// document.getElementById("root").append(heading)
+
+// console.log("JavaScipt elemrnt",heading);
+//React code with JS
+// console.log("React element", reactHeading);
+
+// const reactHeading = React.createElement("h1",{className : "head" , children : "Hello React"},);
+// ReactDOM.createRoot(document.getElementById("root")).render(reactHeading);4
+
+// React with JSX
+
+// const jsxHeading = <>
+//     <h1>Hello JSX</h1>
+//     <p>this is the para wirtten using jsx</p>
+//     </>
+
+// const para = ()
+ReactDOM.createRoot(document.getElementById("root")).render(jsxHeading);
+// ReactDOM.createRoot(document.getElementById("root")).render(para);
+
